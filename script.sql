810f8c73d8bde5e9ffc42508ad0f3fa3f9505c0c
Bin <bindong132@hotmail.com>
2018-04-02 22:03:42 -0400

    added script

---
+++ script.sql
@@ -0,0 +1,95 @@
+
+
+
+
+insert into MemberLevel values(1,"A",1);
+insert into MemberLevel values(2,"B",2);
+insert into MemberLevel values(3,"C",3);
+
+insert into Store values(1,'StoreName_Test','StoreAddressTest','TestType',0,0);
+insert into Store values(1,'StoreName_Test','StoreAddressTest','TestType',1,1);
+insert into Store values(1,'StoreName_Test','StoreAddressTest','TestType',2,2);
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Customer values(1,'First Name','LastName','Email',3,'10');
+insert into Purchase values(1,0,0,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,1,1,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,2,2,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,3,3,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,4,4,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,5,5,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,6,6,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,7,7,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,8,8,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,9,9,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,10,10,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,11,11,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,12,12,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,13,13,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,14,14,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,15,15,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,16,16,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,17,17,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,18,18,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,19,19,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,20,20,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,21,21,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,22,22,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,23,23,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,24,24,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,25,25,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,26,26,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,27,27,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,28,28,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,29,29,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,30,30,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,31,31,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,32,32,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,33,33,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,34,34,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,35,35,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,36,36,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,37,37,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,38,38,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,39,39,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,40,40,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,41,41,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,42,42,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,43,43,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,44,44,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,45,45,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,46,46,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,47,47,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,48,48,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+insert into Purchase values(1,49,49,to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));
+
+
+
+
+insert into Coffee(1,'Promoted','Description',1, 2, 1, 1);
+insert into Coffee(1,'NonPromoted','Description',1, 2, 1, 1);
+
+
+insert into Promotion(1,"Test_Promotion",to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'), to_date('29-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS') );
+
+insert into HasPromotion(1,1)
+insert into PromoteFor(1,1);
+
+insert into BuyCoffee values(1,1,10,10);
---
+++ scriptgenerator.py
@@ -0,0 +1,14 @@
+def generate():
+	f = open("script.txt","w+")
+	for i in range(3):
+		f.write("insert into Store values(1,'StoreName_Test','StoreAddressTest','TestType'," + str(i) + ',' +  str(i) + ');' + "\n")
+
+
+	for i in range(20):
+		f.write("insert into Customer values(1,'First Name','LastName','Email',3,'0');" + "\n")
+
+	for i in range(50):
+		f.write("insert into Purchase values(1," + str(i) + "," +  str(i) + ",to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));" + "\n")
+	f.close()
+generate()
+
