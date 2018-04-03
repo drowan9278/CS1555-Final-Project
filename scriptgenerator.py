5807992aa331aeffd830ca87e8c422910a59819a
Bin <bindong132@hotmail.com>
2018-04-02 23:02:22 -0400

    editted

--- scriptgenerator.py
+++ scriptgenerator.py
@@ -8,7 +8,7 @@ def generate():
 		f.write("insert into Customer values(1,'First Name','LastName','Email',3,'0');" + "\n")
 
 	for i in range(50):
-		f.write("insert into Purchase values(1," + str(i) + "," +  str(i) + ",to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));" + "\n")
+		f.write("insert into Purchase values(1," + str(i+1) + "," +  str(1) + ",to_date('24-FEB-2015 7:00:00','DD-MON-YYYY HH24:MI:SS'));" + "\n")
 	f.close()
 generate()
 
