dac5b176b01811ee3c9d30a5781da14886a96c3b
drowan9278 <drowan9278@students.mc3.edu>
2018-04-21 19:44:12 -0400

    bcdriver

--- sourceCode/src/BCbenchmark.java
+++ sourceCode/src/BCbenchmark.java
@@ -77,10 +77,10 @@ public class BCbenchmark {
         }
 
         System.out.println("Customer Points: "+ db.getPointsByCustomerId(1) + " Coffee Keyword c and o id: "+ db.getCoffeesByKeywords("c","o"));
-        list = db.getCoffeesByKeywords("c","o");
+        list = db.getCoffeesByKeywords("C","o");
         it2 = list.iterator();
         while(it2.hasNext()){
-            System.out.println("Coffee Keywork: "+it2.next());
+            System.out.println("Coffee Keyword: "+it2.next());
         }
         list = db.getCoffees();
         it2 = list.iterator();
